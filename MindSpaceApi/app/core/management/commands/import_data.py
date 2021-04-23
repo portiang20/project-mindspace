bcef7a42729506bb612780a6059c8134e3333180
EricMa <mkyeric@gmail.com>
2021-04-23 10:15:36 +0930

    extract environment constants in django server out and put them into docker-compose.yml, update my timesheet

--- MindSpaceApi/app/core/management/commands/import_data.py
+++ MindSpaceApi/app/core/management/commands/import_data.py
@@ -5,17 +5,14 @@ import os
 from django.contrib.auth import get_user_model
 from datetime import datetime 
 
-U1_UID = 'm78via5KcyPUSe1XJb0Tu3XwcZD3'
-U2_UID = 'h28UH8bcXOVR811IR786r5AlfrJ2'
-
 class Command(BaseCommand):
     """Django command to load csv file into database"""
 
     def handle(self, *args, **options):
         # Delete all emotion records first
         Record.objects.all().delete()
-        u1 = get_user_model().objects.get(uid=U1_UID)
-        u2 = get_user_model().objects.get(uid=U2_UID)
+        u1 = get_user_model().objects.get(uid=os.environ.get('U1_UID'))
+        u2 = get_user_model().objects.get(uid=os.environ.get('U2_UID'))
         with open('mindspace_dataset.csv', encoding='ISO-8859-1') as datafile:
             reader = csv.reader(datafile, delimiter=',')
             for row in reader:
@@ -23,4 +20,6 @@ class Command(BaseCommand):
                     posted_date = datetime.strptime(row[2], '%Y-%m-%d').date()
                     if row[0] == 'U1':
                         Record.objects.create(user=u1,emotion=row[1],posted_date=posted_date,post=row[3])
+                    else:
+                        Record.objects.create(user=u2,emotion=row[1],posted_date=posted_date,post=row[3])
                 
