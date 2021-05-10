637b68b4165d13b9568e2c677841d73c031622f4
EricMa <mkyeric@gmail.com>
2021-05-10 20:03:46 +0930

    Complete modifying the endpoint ‘/api/record/emotions’ to return detected trigger keywords and their occurrence frequency in each record of emotion

--- MindSpaceApi/app/record/serializers.py
+++ MindSpaceApi/app/record/serializers.py
@@ -2,15 +2,21 @@
 
 from rest_framework import serializers
 
-from core.models import Record, Insight
+from core.models import Record, Insight, TriggerKeywordFrequency
 
 class RecordSerializers(serializers.ModelSerializer):
     """Serializer for emotion records objects"""
 
+    trigger_keyword_frequency = serializers.SerializerMethodField()
+
+    def get_trigger_keyword_frequency(self, obj):
+        keyword_freq_objects = TriggerKeywordFrequency.objects.filter(record=obj)
+        return [{"keyword": x.keyword, "frequency": x.frequency} for x in keyword_freq_objects]
+
     class Meta:
         model = Record
-        fields = ('id','emotion','posted_date')
-        read_only_fields = ('id',)
+        fields = ('id', 'emotion', 'posted_date', 'trigger_keyword_frequency','post')
+        read_only_fields = ('id', 'trigger_keyword_frequency','post')
 
 
 class InsightSerializers(serializers.ModelSerializer):
