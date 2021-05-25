8805c430ae2d3db12064e1c71312a19ddd29f9ce
Yingyao Lu <emilylu123@gmail.com>
2021-05-25 17:21:02 +0930

    Update Timeline display emotion & reflection section only if data is available

--- MindSpace/src/app/main/timeline/timeline.page.ts
+++ MindSpace/src/app/main/timeline/timeline.page.ts
@@ -24,6 +24,7 @@ export class TimelinePage implements OnInit {
   isToday: boolean;
   selectedDate: Date = new Date();
   block = false;
+  isShow = false;
 
   emotionsSub: Subscription;
   reflectionsSub: Subscription;
@@ -74,24 +75,8 @@ export class TimelinePage implements OnInit {
     },
   };
 
-  // TODO:Load event log from database
-  loadEvents() {
-    this.eventSource = this.createRandomEvents();
-  }
-
   onViewTitleChanged(title) {
     this.viewTitle = title;
-  }
-
-  onEventSelected(event) {
-    console.log(
-      'Event selected:' +
-        event.startTime +
-        '-' +
-        event.endTime +
-        ',' +
-        event.title
-    );
   }
 
   get reflections() {
@@ -233,73 +218,12 @@ export class TimelinePage implements OnInit {
     this.myCalendar.update();
   }
 
-  onCurrentDateChanged(event: Date) {
-    var today = new Date();
-    today.setHours(0, 0, 0, 0);
-    event.setHours(0, 0, 0, 0);
-    this.isToday = today.getTime() === event.getTime();
-  }
-
-  createRandomEvents() {
-    var events = [];
-    for (var i = 0; i < 50; i += 1) {
-      var date = new Date();
-      var eventType = Math.floor(Math.random() * 2);
-      var startDay = Math.floor(Math.random() * 90) - 45;
-      var endDay = startDay;
-      var startTime;
-      var endTime;
-      if (eventType === 0) {
-        startTime = new Date(
-          Date.UTC(
-            date.getUTCFullYear(),
-            date.getUTCMonth(),
-            date.getUTCDate() + startDay
-          )
-        );
-        if (endDay === startDay) {
-          endDay += 1;
-        }
-        endTime = new Date(
-          Date.UTC(
-            date.getUTCFullYear(),
-            date.getUTCMonth(),
-            date.getUTCDate() + endDay
-          )
-        );
-        events.push({
-          title: 'All Day - ' + i,
-          startTime: startTime,
-          endTime: endTime,
-          allDay: true,
-        });
-      } else {
-        var startMinute = Math.floor(Math.random() * 24 * 60);
-        var endMinute = Math.floor(Math.random() * 180) + startMinute;
-        startTime = new Date(
-          date.getFullYear(),
-          date.getMonth(),
-          date.getDate() + startDay,
-          0,
-          date.getMinutes() + startMinute
-        );
-        endTime = new Date(
-          date.getFullYear(),
-          date.getMonth(),
-          date.getDate() + endDay,
-          0,
-          date.getMinutes() + endMinute
-        );
-        events.push({
-          title: 'Event - ' + i,
-          startTime: startTime,
-          endTime: endTime,
-          allDay: false,
-        });
-      }
-    }
-    return events;
-  }
+  // onCurrentDateChanged(event: Date) {
+  //   var today = new Date();
+  //   today.setHours(0, 0, 0, 0);
+  //   event.setHours(0, 0, 0, 0);
+  //   this.isToday = today.getTime() === event.getTime();
+  // }
 
   ngOnInit() {
     this.emotionsSub = this.emotionsService.emotions.subscribe((records) => {
